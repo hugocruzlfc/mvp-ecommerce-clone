845c28a4a552c6b958476313781b93c68073da53
Hugo Cruz <hugocruzlfc@gmail.com>
2024-07-29 17:37:57 +0200

    feat: add admin page

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,70 @@
+import DashboardCard from "@/components/DashboardCard";
+import { formatCurrency, formatNumber } from "@/lib/formatter";
+import prisma from "@/lib/prisma";
+
+async function getSalesData() {
+  const data = await prisma.order.aggregate({
+    _sum: { pricePaidInCents: true },
+    _count: true,
+  });
+
+  return {
+    amount: (data._sum.pricePaidInCents || 0) / 100,
+    numberOfSales: data._count,
+  };
+}
+
+async function getUserData() {
+  const [userCount, orderData] = await Promise.all([
+    prisma.user.count(),
+    prisma.order.aggregate({
+      _sum: { pricePaidInCents: true },
+    }),
+  ]);
+
+  return {
+    userCount,
+    averageValuePerUser:
+      userCount === 0
+        ? 0
+        : (orderData._sum.pricePaidInCents || 0) / userCount / 100,
+  };
+}
+
+async function getProductData() {
+  const [activeCount, inactiveCount] = await Promise.all([
+    prisma.product.count({ where: { isAvailableForPurchase: true } }),
+    prisma.product.count({ where: { isAvailableForPurchase: false } }),
+  ]);
+
+  return { activeCount, inactiveCount };
+}
+
+export default async function Page() {
+  const [salesData, userData, productData] = await Promise.all([
+    getSalesData(),
+    getUserData(),
+    getProductData(),
+  ]);
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <DashboardCard
+        title="Sales"
+        subtitle={`${formatNumber(salesData.numberOfSales)} Orders`}
+        body={formatCurrency(salesData.amount)}
+      />
+      <DashboardCard
+        title="Customers"
+        subtitle={`${formatCurrency(
+          userData.averageValuePerUser
+        )} Average Value`}
+        body={formatNumber(userData.userCount)}
+      />
+      <DashboardCard
+        title="Active Products"
+        subtitle={`${formatNumber(productData.inactiveCount)} Inactive`}
+        body={formatNumber(productData.activeCount)}
+      />
+    </div>
+  );
+}
---
+++ src/components/DashboardCard.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { title } from "process";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+
+interface DashboardCardProps {
+  title: string;
+  subtitle: string;
+  body: string;
+}
+
+export default function DashboardCard({
+  title,
+  subtitle,
+  body,
+}: DashboardCardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{subtitle}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{body}</p>
+      </CardContent>
+    </Card>
+  );
+}
